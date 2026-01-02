60ffb3592c34bb8811cb837eeb8f95c7929e59d9
ema.cikotic <emacikotic25@gmail.com>
2026-01-02 19:26:29 +0100

    Fruchterman & Reingold Algorithm Implemented

--- src/main/java/org/example/layout/FruchtermanReingold.java
+++ src/main/java/org/example/layout/FruchtermanReingold.java
@@ -1,4 +1,78 @@
 package org.example.layout;
 
+import org.example.graph.Edge;
+import org.example.graph.Graph;
+import org.example.graph.Vertex;
+import org.example.math.Vector2D;
+
 public class FruchtermanReingold {
+
+
+    private Graph graph;
+    private double k;
+    private double temperature;
+
+
+    private double repulsiveForce(double distance) {
+        return (k * k) / distance;
+    }
+    private double attractiveForce(double distance) {
+        return (distance * distance) / k;
+    }
+
+    public FruchtermanReingold( Graph graph, double width, double height ){
+        this.graph=graph;
+        double area= width*height;
+        this.k= Math.sqrt(area/graph.vertices.size());
+        this.temperature=Math.max(width,height)/10;
+    }
+
+    public void step(){
+
+        //step 1: reset displacement
+        for (Vertex v : graph.vertices) v.displacement= new Vector2D (0 ,0);
+
+        //step 2: repulsive forces
+        for (Vertex v : graph.vertices){
+            for (Vertex u: graph.vertices){
+                if (v!= u){
+                    Vector2D delta= v.position.subtract(u.position);
+                    double distance=delta.length(); //how far apart are the vertices
+
+                    //potential overflow safeguard
+                    if (distance>0){
+                        Vector2D force= delta.normalize().multiply(repulsiveForce(distance));
+                        v.displacement=v.displacement.add(force);
+                    }
+                }
+            }
+        }
+
+        //step 3: attractive forces
+        for (Edge e: graph.edges){
+            Vertex v = e.v;
+            Vertex u = e.u;
+            Vector2D delta=v.position.subtract(u.position);
+            double distance= delta.length();
+
+            if (distance>0){
+                Vector2D force=delta.normalize().multiply(attractiveForce(distance));
+                v.displacement = v.displacement.subtract(force);
+                u.displacement = u.displacement.add(force);
+            }
+        }
+
+        //step 4: adjust displacement
+        for (Vertex v : graph.vertices){
+            double displLength=v.displacement.length();
+
+            if (displLength > 0){
+                Vector2D move = v.displacement.normalize().multiply(Math.min(displLength,temperature));
+                v.position = v.position.add(move);
+            }
+        }
+
+        temperature*=0.95;
+    }
+
 }

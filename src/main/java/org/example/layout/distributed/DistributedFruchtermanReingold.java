3b71dccea7bec9d0b70e5feb2de1cfc07483a85d
ema.cikotic <emacikotic25@gmail.com>
2026-06-30 22:25:34 +0200

    Distributed skeleton.

--- src/main/java/org/example/layout/distributed/DistributedFruchtermanReingold.java
+++ src/main/java/org/example/layout/distributed/DistributedFruchtermanReingold.java
@@ -1,4 +1,28 @@
 package org.example.layout.distributed;
 
-public class DistributedFruchtermanReingold {
+import org.example.graph.Graph;
+
+import org.example.layout.LayoutAlgorithm;
+
+public class DistributedFruchtermanReingold implements LayoutAlgorithm {
+    private Graph graph;
+    private double k;
+    private  double temperature;
+    private double height;
+    private double width;
+
+    public DistributedFruchtermanReingold(Graph graph, double width, double height, double c) {
+        this.graph = graph;
+        double area = width * height;
+        this.k = c * Math.sqrt(area / graph.vertices.size());
+        this.temperature = Math.max(width, height) / 50;
+        this.height = height;
+        this.width = width;
+    }
+    @Override
+    public void step() {
+        throw new UnsupportedOperationException(
+                "Distributed implementation not yet available."
+        );
+    }
 }

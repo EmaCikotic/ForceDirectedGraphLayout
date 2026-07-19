f5e95e8252a7120b66f698f28f2a9a8a94184702
ema.cikotic <emacikotic25@gmail.com>
2026-07-19 02:00:07 +0200

    additional configurations for 1-4 processes to compare the results

--- src/main/java/org/example/benchmark/DistributedPerformanceBenchmark.java
+++ src/main/java/org/example/benchmark/DistributedPerformanceBenchmark.java
@@ -77,7 +77,7 @@ public class DistributedPerformanceBenchmark {
         try {
 
             if (rank == 0){
-                writer = new PrintWriter("distributed_benchmark_results.csv");
+                writer = new PrintWriter("distributed_benchmark_" + processes + "_processes.csv"); //so different number of processes get saved to different files
                 writer.println(
                         "experiment," +
                                 "vertices," +
